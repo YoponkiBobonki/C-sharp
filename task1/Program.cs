1f0d227d5887dce2487620a9a74c1b7039ef5660
Miron <Miran113099@gmail.com>
2024-08-30 21:42:35 +0900

    add Lesson2:task1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int [5];
+arr[0] = 5;
+arr[1] = 3;
+arr[2] = 2;
+arr[3] = 57;
+arr[4] = 52;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
